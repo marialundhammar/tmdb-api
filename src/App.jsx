63e65f50b64454a7710c3c4920ba238ef99399bd
Maria Lundhammar <marialundhammar@gmail.com>
2022-08-17 13:53:34 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { Routes, Route } from 'react-router-dom'
-import Navigation from './components/Navigation'
-import HomePage from './pages/HomePage'
-import NotFound from './pages/NotFound'
-import './assets/scss/App.scss'
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import HomePage from './pages/HomePage';
+import NotFound from './pages/NotFound';
+import './assets/scss/App.scss';
 
 function App() {
-	return (
-		<div id="App">
-			<Navigation />
+  return (
+    <div id='App'>
+      <Navigation />
 
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</div>
-	)
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        /* <Route path='*' element={<NotFound />} /> */
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
